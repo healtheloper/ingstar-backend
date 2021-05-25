aaa15a1846e42db199cf0f46ab686ca07cb058b5
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-05-25 21:38:44 +0900

    Setup: Prisma Setup and only server.js

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { PrismaClient } from ".prisma/client";
+import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
-  type Query {
-    hello: String
+  type Query: {
+   movies: [Movie],
+   movie(id:Int!): Movie
+  },
+  type Mutation {
+    createMovie(title:String!, year:Int!, genre:String): Movie
+    deleteMovie(id:Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({ data: { title, year, genre } }),
+    deleteMobie: (_, { id }) => client.movie.delete({ where: { id } }),
   },
 };
 
