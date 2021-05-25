d7761a2eb485b791acc1dabdf3617a97bc17ad2c
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-05-25 22:18:44 +0900

    Setup: type fix and update, Prisma Studio

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ import { ApolloServer, gql } from "apollo-server";
 const client = new PrismaClient();
 
 const typeDefs = gql`
-  type Query: {
-   movies: [Movie],
-   movie(id:Int!): Movie
-  },
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
   type Mutation {
-    createMovie(title:String!, year:Int!, genre:String): Movie
-    deleteMovie(id:Int!): Movie
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
@@ -22,7 +31,9 @@ const resolvers = {
   Mutation: {
     createMovie: (_, { title, year, genre }) =>
       client.movie.create({ data: { title, year, genre } }),
-    deleteMobie: (_, { id }) => client.movie.delete({ where: { id } }),
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({ where: { id }, data: { year } }),
   },
 };
 
